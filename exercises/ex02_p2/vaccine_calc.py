402da0b07953162bfc11920aa6bcbfd524d3faea
Mackenzie Wittmer <kenziewittmer@gmail.com>
2021-02-16 13:21:34 -0500

    Update on vaccine_calc

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 __author__ = "730405231"
 
 
-def main() -> None:
+def main() -> None: 
     """The entrypoint of the program, when run as a module."""
     population: int = int(input("Population: "))
     doses: int = int(input("Doses administered: "))
@@ -16,18 +16,22 @@ def main() -> None:
     print("We will reach " + str(target) + "% vaccination in " + str(x) + " days, which falls on " + y + ".")
 
 
-def days_to_target(population:int, doses:int, doses_per_day:int, target:int) -> int:
-    doses = (population * 2) - doses
-    a: int = int(doses * (target / 100) / doses_per_day)
-    return a
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int: 
+    """Calculates number of days left until target vaccine population is reached!"""
+    doses = int(((population * 2) * (target / 100)) - doses)
+    a: float = doses / doses_per_day
+    b: int = round(a)
+    return b
 
 
-def future_date(number_days:int) -> str:
+def future_date(number_days: int) -> str: 
+    """Converts number of days from today to a future date!"""
     today: datetime = datetime.today()
     days_to_goal: timedelta = timedelta(number_days)
     date_reach_goal: datetime = today + days_to_goal
     final_date: str = date_reach_goal.strftime("%B %d, %Y")
     return final_date
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     main()
